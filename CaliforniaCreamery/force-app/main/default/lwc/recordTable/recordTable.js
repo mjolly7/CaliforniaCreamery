0a3b855a9060e80d9f1fb7b6269c3806042cb74f
= <mj.jolly7@gmail.com>
2023-10-14 20:13:57 -0700

    refactor. Add sorting

--- CaliforniaCreamery/force-app/main/default/lwc/recordTable/recordTable.js
+++ CaliforniaCreamery/force-app/main/default/lwc/recordTable/recordTable.js
@@ -1,3 +1,65 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
 
-export default class RecordTable extends LightningElement {}
+export default class RecordTable extends LightningElement {
+    @api rows;
+    @api colNames;
+
+    @track sortedField;
+    @track sortDirection = 'asc';
+    @track sortedRows;
+    @track columns;    
+
+    connectedCallback() {
+        //Objects passed from parent can't be modified directly. Use spread operator to 
+            //create a new copy of the array 
+        this.sortedRows = [...this.rows];
+        
+        //Add sorting control variables to column
+        this.columns = [];
+        this.colNames.forEach(cn => {
+            this.columns.push({"name":cn, "isSortedAsc":false, "isSortedDesc":false});
+        });
+    }
+
+    @api applyFilter(filteredStores) {
+        this.sortedRows = [...filteredStores];
+    }
+
+    sortColumn(e) {
+        let fieldName = e.target.dataset.type;
+        this.sortedField = fieldName;
+        this.sortDirection = this.sortDirection == 'asc' ? 'desc' : 'asc';
+
+        //Sort the rows by selected column
+        this.sortedRows.sort((a,b) => {
+            if(this.sortDirection == 'asc') {
+                if(a[fieldName] > b[fieldName]) {
+                    return 1;
+                } else if(a[fieldName] < b[fieldName]) {
+                    return -1;
+                } else {
+                    return 0
+                }
+            } else {
+                if(a[fieldName] > b[fieldName]) {
+                    return -1;
+                } else if(a[fieldName] < b[fieldName]) {
+                    return 1;
+                } else {
+                    return 0
+                }
+            }
+        });
+        
+        //Add sorted column direction indicator
+        this.columns.forEach(col => {
+            if(col.name == this.sortedField) {
+                col.isSortedAsc = this.sortDirection == 'asc';
+                col.isSortedDesc = this.sortDirection == 'desc';
+            } else {
+                col.isSortedAsc = false;
+                col.isSortedDesc = false;
+            }
+        });
+    }
+}
--- CaliforniaCreamery/force-app/main/default/lwc/storeRecordTable/storeRecordTable.js
+++ CaliforniaCreamery/force-app/main/default/lwc/storeRecordTable/storeRecordTable.js
@@ -11,29 +11,37 @@ export default class StoreRecordTable extends LightningElement {
     
     @track totalAmount = 0;
     @track showTotalAmount = false;
+    @track tableHasData = false;
 
-    @track sortDirection = 'asc';
+
+   
 
     connectedCallback() {
         this.isLoading = true;
-        console.log('!!!!!!!!!!!!!!!!!!!!!');
         fetchTableInfo().then((data,error) => {
             let baseUrl = window.location.origin;
             if(data) {
-                console.log('data::: ' + JSON.stringify(data));
                 let dataWrapper = JSON.parse(data);
                 this.stores = dataWrapper.Stores;
+                this.tableHasData = dataWrapper.Stores.length > 0;
                 this.filteredStores = this.stores;
+
+                
+                //Convert the values to list of fields
+                    //This supports iterating over the column values to support dynamic tables
                 this.filteredStores.forEach(store=> {
                     store.url = baseUrl + '/' + store.Id;
+                    store.fieldValues = [];
+                    store.fieldValues.push({"value":store.City});
+                    store.fieldValues.push({"value":store.State});
+                    store.fieldValues.push({"value":store.Amount,"isCurrency":true});
                 });
+
+                //Add city names to picklist options
                 dataWrapper.GeoInfo.Cities.forEach((cityName) => {
-                    console.log('cityName:::: ' + JSON.stringify(cityName));
-                    this.cities.push(cityName);
-                    
+                    this.cities.push(cityName);                    
                 });
-                console.log('@@@@@@@@@@@@@@@@@@@@@@');
-                console.log('this.cities:::: ' + JSON.stringify(this.cities));
+                //this.cities.sort();
             } else if(error) {
                 console.log('error::: ' + JSON.stringify(error));
             } else {
@@ -41,45 +49,21 @@ export default class StoreRecordTable extends LightningElement {
             }
             this.isLoading = false;
         });
-    }
-
-    sortColumn(e) {
-        console.log('111111111');
-        console.log('sorting currtarget::: ' + e.currentTarget.dataset.type);
-        console.log('sorting target::: ' + e.target.dataset.type);
-        let fieldName = e.target.dataset.type;
-        this.sortDirection = this.sortDirection == 'asc' ? 'desc' : 'asc';
-        this.filteredStores.sort((a,b) => {
-            console.log('a::: ' + a[fieldName]);
-            console.log('b::: ' + b[fieldName]);
-            console.log('this.sortDirection::: ' + this.sortDirection);
-            console.log(a[fieldName] > b[fieldName]);
-            console.log(b[fieldName] < a[fieldName]);
-            if(this.sortDirection == 'asc') {
-                return a[fieldName] > b[fieldName];
-            } else {
-                return b[fieldName] < a[fieldName];
-            }            
-        });
-        console.log('this.filteredStores::: ' + JSON.stringify(this.filteredStores));
-        
-        
-    }
+    }    
 
     handleCityChange(e) {
-        console.log('changing city:::::  ' + JSON.stringify(e));
-        console.log('value:: ' + e.target.value);
         this.selectedCity = e.target.value;
         if(this.selectedCity == '-') {
             this.showTotalAmount = false;
             this.filteredStores = this.stores;
         } else {
+            //Only show total amount when the city filter is applied
             this.showTotalAmount = true;
             this.filteredStores = this.stores.filter((store) => store.City == this.selectedCity);
             this.totalAmount = this.filteredStores.reduce(function(accumulator,currentStore) {return accumulator + currentStore.Amount;},0);
-            console.log('amount:::: ' + JSON.stringify(this.totalAmount));
         }
-        console.log('this.filteredStores ~~~~~~> ' + JSON.stringify(this.filteredStores));
 
+        //Refresh the child table component
+        this.template.querySelector("c-record-table").applyFilter(this.filteredStores);
     }
 }
